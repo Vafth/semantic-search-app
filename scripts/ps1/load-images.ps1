dad0172d88aa58c3920de4204fd47bdcf32cafb8
Vafth <nuuskamuikkunen671@gmail.com>
2026-03-22 17:14:12 +0200

    chore: rename images to semantic-search prefix, change gateway service type to LoadBalancer

--- scripts/ps1/load-images.ps1
+++ scripts/ps1/load-images.ps1
@@ -1,8 +1,7 @@
 Write-Host "Loading images into Minikube..."
-minikube image load 703-document-service:latest
-minikube image load 703-search-service:latest
-minikube image load 703-gateway:latest
-minikube image load 703-model-service:latest
+minikube image load semantic-search-model-service:latest
 minikube image load qdrant/qdrant:v1.17.0
-minikube image load nginx:alpine
+minikube image load semantic-search-document-service:latest
+minikube image load semantic-search-search-service:latest
+minikube image load semantic-search-gateway:latest
 Write-Host "Done."
