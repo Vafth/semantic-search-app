cbb1d69286d54af817695f4784e5514064ff8023
Vafth <nuuskamuikkunen671@gmail.com>
2026-07-16 20:32:25 +0200

    chore: remove legacy minikube manifests and deployment scripts

--- scripts/ps1/load-images.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Loading images into Minikube..."
-minikube image load semantic-search-model-service:latest
-minikube image load qdrant/qdrant:v1.17.0
-minikube image load semantic-search-document-service:latest
-minikube image load semantic-search-search-service:latest
-minikube image load semantic-search-gateway:latest
-Write-Host "Done."
--- scripts/sh/reset-minikube.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Deleting all resources..."
-kubectl delete -f minikube/services/
-kubectl delete -f minikube/deployments/
-kubectl delete -f minikube/volumes/
-kubectl delete -f minikube/configmaps/
-echo "Done."
