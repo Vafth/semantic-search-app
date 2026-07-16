634c99087dbca76ef5f1dfe9e8751d981d3b8300
Vafth <nuuskamuikkunen671@gmail.com>
2026-07-16 20:35:27 +0200

    feat(infra): introduce terraform and argocd manifests

---
+++ terraform/secrets.tf
@@ -0,0 +1,24 @@
+# postgres-secret.tf
+resource "kubernetes_secret" "postgres_secret" {
+  metadata {
+    name      = "postgres-secret"
+    namespace = kubernetes_namespace.semantic_search.metadata[0].name
+  }
+  data = {
+    POSTGRES_DB       = var.postgres_db
+    POSTGRES_USER     = var.postgres_user
+    POSTGRES_PASSWORD = var.postgres_password
+    POSTGRES_URL      = "postgresql+asyncpg://${var.postgres_user}:${var.postgres_password}@postgres:5432/${var.postgres_db}"
+  }
+}
+
+# auth-secret.tf
+resource "kubernetes_secret" "auth_secret" {
+  metadata {
+    name      = "auth-secret"
+    namespace = kubernetes_namespace.semantic_search.metadata[0].name
+  }
+  data = {
+    SECRET_KEY = var.secret_key
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "postgres_db" {
+  description = "PostgreSQL database name"
+  default     = "project_db"
+}
+
+variable "postgres_user" {
+  description = "PostgreSQL user"
+  default     = "admin"
+}
+
+variable "postgres_password" {
+  description = "PostgreSQL password"
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "Secret Key for AUTH"
+  sensitive   = true
+}
