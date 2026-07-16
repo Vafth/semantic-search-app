ce5203724afb90b101f568d541b0332479df296c
Vafth <nuuskamuikkunen671@gmail.com>
2026-07-16 20:35:27 +0200

    feat(infra): introduce terraform and argocd manifests

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+
+resource "kubernetes_namespace" "semantic_search" {
+  metadata {
+    name = "semantic-search"
+  }
+}
